3275247e90014232cdab4a4cf8e565504be002c9
ABDULAZIZ ALRASHEDI <bogico.sa@gmail.com>
2026-04-10 16:01:11 +0300

    Node_JS_basic tasks

--- Node_JS_basic/.eslintrc.js
+++ Node_JS_basic/.eslintrc.js
@@ -1,19 +1,37 @@
 module.exports = {
   env: {
+    browser: false,
     es6: true,
-    node: true,
     jest: true,
   },
-  extends: ['airbnb-base', 'plugin:jest/recommended'],
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
   plugins: ['jest'],
   rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
     'no-console': 'off',
-    'import/extensions': ['error', 'ignorePackages', {
-      js: 'never',
-    }],
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    },
+  ],
 };
